bf7e530e005286af1b2fea80ba4b52960e7fb5a3
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2023-05-01 16:41:41 +0100

    adding icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,19 +25,81 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
         <link rel="manifest" href="/manifest.json" />
         <link
-          href="/icons/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="icons/apple-icon-57x57.png"
         />
         <link
-          href="/icons/favicon-32x32.png"
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="icons/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="icons/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="icons/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="icons/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="icons/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="icons/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="icons/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="icons/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="icons/android-icon-192x192.png"
+        />
+        <link
           rel="icon"
           type="image/png"
           sizes="32x32"
+          href="icons/favicon-32x32.png"
         />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="icons/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="icons/favicon-16x16.png"
+        />
+
+        <meta name="msapplication-TileColor" content="#111827" />
+        <meta
+          name="msapplication-TileImage"
+          content="icons/ms-icon-144x144.png"
+        />
+        <meta name="theme-color" content="#0f1526" />
       </Head>
       <RecoilRoot>
         <Component {...pageProps} />
